178acc0e949a7da9aa0ffc8f04d25fbc23cf0f45
Kassim Sheghembe <sheghembekassim@gmail.com>
2024-03-28 09:08:56 +0300

    Refactor a model for the indicator reason for choosing a location i.e ADDO vs HF

---
+++ models/disf_data/reason_choosing_location.sql
@@ -0,0 +1,37 @@
+SELECT
+    ahvfm.location_id,
+    ahvfm.event_date,
+    ahvfm.client_type,
+    ahvfm.ward_id,
+    ahvfm.ward_name,
+    ahvfm.district_id,
+    ahvfm.district_name,
+    coalesce(
+        t2.english,
+        ahvfm.source_selection_medicine
+    ) AS reason_for_selecting_addo,
+    coalesce(
+        t.english,
+        ahvfm.source_medicine
+    ) AS source_of_meds,
+    count(*) AS count
+FROM
+    afyatek_disf_data.chw_home_visit_meds AS ahvfm
+LEFT JOIN translations.translations AS t
+    ON
+        ahvfm.source_medicine = t.swahili
+LEFT JOIN translations.translations AS t2
+    ON
+        ahvfm.source_selection_medicine = t2.swahili
+WHERE
+    ahvfm.event_date >= '2024-03-01'
+GROUP BY
+    reason_for_selecting_addo,
+    source_of_meds,
+    ahvfm.location_id,
+    ahvfm.event_date,
+    ahvfm.client_type,
+    ahvfm.ward_id,
+    ahvfm.ward_name,
+    ahvfm.district_id,
+    ahvfm.district_name
