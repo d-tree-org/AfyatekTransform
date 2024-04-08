b649cbc3888d11eaddad7ce64402e62319ad55cd
nitusima <nkataraia@d-tree.org>
2024-04-08 16:29:37 +0300

    added date filter

--- models/visits/all_addo_visits.sql
+++ models/visits/all_addo_visits.sql
@@ -102,4 +102,4 @@ LEFT JOIN
     {{ source('location_data', 'openmrs_location_mapping_final') }} AS loc
     ON loc.location_id = coalesce(anc.location_id, pnc.location_id, child.location_id)
 WHERE
-    coalesce(anc.event_date, pnc.event_date, child.event_date) >= '2022-01-01'::date
+    coalesce(anc.event_date, pnc.event_date, child.event_date) >= '2024-03-01'::date
