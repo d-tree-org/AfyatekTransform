5d221c6872fbe8a851c74ef0728ae2930cbbae63
Kassim Sheghembe <sheghembekassim@gmail.com>
2024-08-14 13:33:49 +0300

    Added child registration and family member registration models.

---
+++ models/registrations/family_member.sql
@@ -0,0 +1,27 @@
+{%- set fields = [
+    {"name": "start", "type": "timestamp","rename":"start_time"},
+    {"name": "end", "type": "timestamp","rename":"start_time"},
+    {"name": "deviceid"},
+    {"name": "simserial"},
+    {"name": "subscriberid"},
+    {"name": "service_provider"},
+    {"name": "fam_name"},
+    {"name": "age_calculated"},
+    {"name": "dob_calculated"},
+    {"name": "wra"},
+    {"name": "mra"},
+    {"name": "is_primary_caregiver"},
+    {"name": "last_interacted_with"},
+    {"name": "member_consent"},
+    {"name": "same_as_fam_name"},
+    {"name": "finger_print"},
+    {"name": "dob_unknown"},
+    {"name": "preferred_name"},
+    {"name": "disabilities"},
+    {"name": "other_phone_number"},
+    {"name": "preg_1yr"},
+    {"name": "fam_consent"},
+    {"name": "consent_photo"}
+] 
+-%}
+{{- flattern_obs('family_member_registration',fields) -}}
